af24160ba8506978947da6d4b18719733cdfcbc9
Кирилл Зайцев <moja.orbita@ya.ru>
2022-12-10 17:51:19 +0300

    Добавьте файлы проекта.

---
+++ Lab6_HW_Task_Weather/WeatherControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Lab6_HW_Task_Weather
+{
+    enum Pre { sunny, cloudy, rain, snow }
+
+    class WeatherControl : DependencyObject
+    {
+        private Pre pre;
+        private string windDerection;
+        private int windSpeed;
+        public string WindDirection { get; private set; }
+        public int WindSpeed { get; private set; }
+
+        public WeatherControl(Pre pre, string windDerection, int windSpeed)
+        {
+            this.pre = pre;
+            this.windDerection = windDerection;
+            this.WindSpeed = windSpeed;
+        }
+        public static readonly DependencyProperty DataProperty;
+        public int Data { get => (int)GetValue(DataProperty); set => SetValue(DataProperty, value); }
+
+        static WeatherControl()
+        {
+            DataProperty = DependencyProperty.Register(
+                nameof(Data),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.Inherits,
+                    null,
+                    new CoerceValueCallback(CoerceData)),
+                new ValidateValueCallback(ValideDate));
+        }
+
+        private static bool ValideDate(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50) { return true; }
+            else { return false; }
+        }
+
+        private static object CoerceData(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50) { return v; }
+            else { return null; }
+        }
+    }
+}
---
+++ Wpf_Lab6/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wlesson_Task1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    class Person : DependencyObject
+    {
+        public static readonly DependencyProperty AgeProperty;
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => (int)GetValue(AgeProperty);
+            set => SetValue(AgeProperty, value);
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        static Person()
+        {
+            AgeProperty = DependencyProperty.Register(
+                nameof(Age),
+                typeof(int),
+                typeof(Person),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceAge)),
+                new ValidateValueCallback(ValidateAge));
+        }
+
+        private static bool ValidateAge(object value)
+        {
+            int v = (int)value;
+            if (v >= 0 && v < 200) { return true; }
+            else { return false; }
+        }
+
+        private static object CoerceAge(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= 0 && v < 200) { return v; }
+            else { return 0;}           
+        }
+
+        public string Print()
+        {
+            return $"{Name}{Age}";
+        }
+    }
+}
+
